eff3737d97868dd8ec39cbc90c564fb5052ea94b
Valiantsin <valicser@yandex.ru>
2019-05-12 11:49:03 +0300

    feat: firebase auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import { app } from './firebase.config';
+
+import firebase from 'firebase';
+
+export const App = () => {
+
+  const [user, setUser] = useState<firebase.User | null>();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(function(user) {
+      setUser(user);
+    });
+  }, [])
+
+  const handleAuth = useCallback(async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    await app.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    const result =  await app.auth().signInWithPopup(provider)
+    setUser(result.user);
+  }, []);
+
+  const handleLogout = useCallback(async () => {
+    setUser(undefined);
+    await app.auth().signOut();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +43,15 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        {user ? (
+          <>
+            <div>{user.email}</div>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+            <button onClick={handleAuth}>Auth Google</button>
+          )}
       </header>
     </div>
   );
 }
-
-export default App;
